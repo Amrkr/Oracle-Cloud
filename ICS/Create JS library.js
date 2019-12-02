e0444a87d638ad500bbf51d8792253e6fd3a2355
Amar Kumar <amrkrcse@gmail.com>
2019-12-02 15:36:43 +0530

    Create Create JS library.js

---
+++ ICS/Create JS library.js
@@ -0,0 +1,14 @@
+function funcOne(param1) {
+ return param1;
+}
+function funcTwo(param2){
+ return param2
+}
+function myFunc(param1, param2) {
+ var ret = funcOne(param1) + funcTwo(param2);
+ return ret;
+}
+
+
+
+//URl = https://blogs.oracle.com/integration/using-a-library-in-oic
